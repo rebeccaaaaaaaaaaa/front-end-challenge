95fb6616c237cec2b986d94147767ca7499423c4
rebecca <rebeccaignacio1996@gmail.com>
2022-02-01 23:00:54 -0300

    ajustes nos detalhes - detalhes ja está funcionando

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import {DetailsTitle, DetailsText} from './style';
+import Moment from 'react-moment';
 
 function MovieDetails(props) {
   return (
-    <div className="container">
-     <div className="row">
-     <div className="col-lg-6">
-      <h1>{props.currentMovie.title}</h1>
-      <img src={`https://image.tmdb.org/t/p/w500${props.currentMovie.poster_path}`} alt={props.currentMovie.title} />
-      
-      </div>
-      <div className="col-lg-6">
-      <p>{props.currentMovie.overview}</p>
-      <p>{props.currentMovie.release_date}</p>
-      <p>{props.currentMovie.vote_average}</p>
-      </div>
-     </div>
-    </div>
+    <div className="container py-5">
+      <div className="row">
+        <div className="col-lg-4 col-md-12">
+        <div class="card">
+        <img
+            src={`https://image.tmdb.org/t/p/w500${props.currentMovie.poster_path}`}
+            alt={props.currentMovie.title}
+          />
+          <div class="card-body">
+            <h5 class="card-title">{props.currentMovie.title}</h5>
+            <p>
+            {" "}
+            Data de lançamento:
+            <Moment format="D MMM YYYY" withTitle>
+              {props.currentMovie.release_date}
+            </Moment>
+          </p>
+          <p> Classificação: {props.currentMovie.vote_average}</p>
+          <p> Idioma original: {props.currentMovie.original_language}</p>
+          </div>
+        </div>
+        </div>
 
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/MovieDetails/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const DetailsTitle = styled.h1`
+    font-size: 2.5rem;
+    font-weight: bold;
+    margin: unset;
+    margin-bottom: 1rem;
+`;
+
+export const DetailsText = styled.p`
+    font-size: 1.5rem;
+    margin-top: 4rem;
+`;
