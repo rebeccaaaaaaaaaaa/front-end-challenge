316e751dc65a9ae00348d0676a4375003a64496a
rebecca <rebeccaignacio1996@gmail.com>
2022-02-04 01:19:41 -0300

    ajustes no visual geral do app

--- src/components/Painel/style.js
+++ src/components/Painel/style.js
@@ -14,4 +14,20 @@ export const MovieCard = styled.div`
     &:hover{
         opacity: 0.8;
     }
+
+    span{
+        width: 40px;
+        height: 40px;
+        border-radius: 40px;
+        background: linear-gradient(to top right, #5c16c5, #2d0c5e);
+        color: #fff;
+        font-size: 12px;
+        line-height: 40px;
+        position: absolute;
+        text-align: center;
+        right: -15px;
+        top: 0;
+
+      
+    }
 `;
